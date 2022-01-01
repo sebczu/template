28e9488528355ca6d00c698f10fee9a1f1cadb05
sebczu <dev.sebastian.czubala@gmail.com>
2022-01-01 15:49:41 +0100

    bean scan improvement

--- integration-test/it-actuator/src/test/java/com/sebczu/template/actuator/SpringBootIntegrationTest.java
+++ integration-test/it-actuator/src/test/java/com/sebczu/template/actuator/SpringBootIntegrationTest.java
@@ -5,7 +5,7 @@ import static java.util.TimeZone.getTimeZone;
 import java.util.TimeZone;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.sebczu.template")
+@SpringBootApplication(scanBasePackages = "com.sebczu")
 public class SpringBootIntegrationTest {
 
   static {
